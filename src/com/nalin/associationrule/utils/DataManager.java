a9d6b5182d66b289df35a1c2a643795c27d15682
Nalin Adhikari <csitplay@gmail.com>
2017-01-17 21:13:42 +0545

    removeOutNumber() method is created for eliminating useless data item.

--- src/com/nalin/associationrule/utils/DataManager.java
+++ src/com/nalin/associationrule/utils/DataManager.java
@@ -3,10 +3,13 @@
  */
 package com.nalin.associationrule.utils;
 
+import java.util.Map;
+
 /**
  * @author nalin
  *
  */
 public interface DataManager {
 
+	public Map<String, Integer> removeOutNumber(Map<String, Integer> itemset);
 }
--- src/com/nalin/associationrule/utils/DataManagerImpl.java
+++ src/com/nalin/associationrule/utils/DataManagerImpl.java
@@ -3,9 +3,28 @@
  */
 package com.nalin.associationrule.utils;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author nalin
  *
  */
-public class DataManagerImpl implements DataManager{
+public class DataManagerImpl implements DataManager {
+
+	@Override
+	public Map<String, Integer> removeOutNumber(Map<String, Integer> itemset) {
+
+		Map<String, Integer> newItemset = new TreeMap<String, Integer>();
+
+		itemset.forEach((k, v) -> {
+			if (v > 1) {
+				newItemset.put(k, v);
+			}
+		});
+
+		itemset = null;
+
+		return newItemset;
+	}
 }
